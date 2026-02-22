61791dc2c21170fb5b97da3c42eb43c008c69649
Kshitij Goel <kshitijgoel33@gmail.com>
2026-02-22 20:27:44 +0530

    Basic Structure

---
+++ lockinvro.py
@@ -0,0 +1,9 @@
+import random
+
+word1 = ["grind","lock in for","channelise those vibes","channel them brainwaves","go ballistic","go ham","go hard","go off","go savage","go supernova","go wild","hustle","keep it 100","keep it real","keep it 100%","keep it 100 percent","keep it one hundred","keep it one hundred percent","keep it one hundred percent real","keep it one hundred real","keep it one hundred true","keep it true","lock in for the grind","lock in for the hustle","lock in for the work","lock in for the win","put in work","go all in"]
+test = str(input("What u studying for? : "))
+
+def lockinvro():
+    print("you gotta",random.choice(word1), "for this",test,"vro.")
+
+lockinvro()
