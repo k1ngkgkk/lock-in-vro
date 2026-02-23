931e997227d1abaa122b9f25a39e109faebd2ba9
k1ngkgkk <kshitijgoel33@gmail.com>
2026-02-23 16:14:17 +0530

    test

--- code/lockinvro.py
+++ code/lockinvro.py
@@ -1,9 +1,25 @@
 import random
 
-word1 = ["grind","lock in for","channelise those vibes","channel them brainwaves","go ballistic","go ham","go hard","go off","go savage","go supernova","go wild","hustle","keep it 100","keep it real","keep it 100%","keep it 100 percent","keep it one hundred","keep it one hundred percent","keep it one hundred percent real","keep it one hundred real","keep it one hundred true","keep it true","lock in for the grind","lock in for the hustle","lock in for the work","lock in for the win","put in work","go all in"]
-test = str(input("What u studying for? : "))
+word = [
+    "grind",
+    "lock in for",
+    "channelise those vibes",
+    "channel them brainwaves",
+    "go ballistic",
+    "go ham",
+    "go hard",
+    "go off",
+    "go savage"
+    "seama":
+]
 
-def lockinvro():
-    print("you gotta",random.choice(word1), "for this",test,"vro.")
+template = ["You gotta {phrase} this {subject} vro."]
 
-lockinvro()
+def lockinvro(subject):
+    phrase = random.choice(word)
+    tmp = random.choice(template)
+    print(tmp.format(phrase=phrase, subject=subject))
+
+if __name__ == "__main__":
+    test = str(input("What u studying for vro? : "))
+    lockinvro(test)
