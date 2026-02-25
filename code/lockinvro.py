1930426ef24211705d60b59039fe24db80261288
k1ngkgkk <kshitijgoel33@gmail.com>
2026-02-25 18:51:17 +0530

    learnt how to show a random asthetic image and did it cuz im like that

--- code/lockinvro.py
+++ code/lockinvro.py
@@ -1,4 +1,7 @@
 import random
+import requests
+from PIL import Image, ImageDraw, ImageFont
+from io import BytesIO
 
 word = [
     "grind",
@@ -12,13 +15,31 @@ word = [
     "go savage"
 ]
 
-template = ["You gotta {phrase} this {subject} vro."]
+imgUrl = "https://source.unsplash.com/1080x1080/?aesthetic,study,light"
+response = requests.get(imgUrl)
+img = Image.open(BytesIO(response.content)).convert("RGB")
+
+
+template = ["You gotta {phrase} this {subject} vro.",
+            "Vro, you gotta {phrase} this {subject}.",
+            "You gotta {phrase} for this {subject} vro.",
+            "Vro, you gotta {phrase} for this {subject}.",
+            "I trust you can {phrase} this {subject} vro."]
 
 def lockinvro(subject):
     phrase = random.choice(word)
     tmp = random.choice(template)
-    print(tmp.format(phrase=phrase, subject=subject))
+    return(tmp.format(phrase=phrase, subject=subject))
 
 if __name__ == "__main__":
     test = str(input("What u studying for vro? : "))
     lockinvro(test)
+
+draw = ImageDraw.Draw(img)
+font = ImageFont.truetype("arial.ttf", 40)
+text = lockinvro(test)
+text_width, text_height = draw.textsize(text, font=font)
+x = (img.width - text_width) / 2
+y = (img.height - text_height) / 2
+draw.text((x, y), text, font=font, fill="white")
+img.show()
